b21b366501c6e9181be9e285336841f5544a6989
bandi <bandi12424@naver.com>
2023-04-21 14:09:16 +0900

    [add] step42.py

--- dezero/utils.py
+++ dezero/utils.py
@@ -140,3 +140,25 @@ def sum_to(x, shape):
     if lead > 0:
         y = y.squeeze(lead_axis)
     return y
+
+
+def reshape_sum_backward(gy, x_shape, axis, keepdims):
+    """dezero.functions.sum의 역전파 수행을 위해 그레디언트를 reshape
+    """
+    ndim = len(x_shape)
+    tupled_axis = axis
+    if axis is None:
+        tupled_axis = None
+    elif not isinstance(axis, tuple):
+        tupled_axis = (axis,)
+
+    if not (ndim == 0 or tupled_axis is None or keepdims):
+        actual_axis = [a if a >= 0 else a + ndim for a in tupled_axis]
+        shape = list(gy.shape)
+        for a in sorted(actual_axis):
+            shape.insert(a, 1)
+    else:
+        shape = gy.shape
+
+    gy = gy.reshape(shape)  # reshape
+    return gy
