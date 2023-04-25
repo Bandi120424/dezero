a05f048077ee887520a60a84da0b0206eb37fff2
bandi <bandi12424@naver.com>
2023-04-25 15:11:25 +0900

    feat: delay weight initialization

--- dezero/layers.py
+++ dezero/layers.py
@@ -2,9 +2,9 @@ import os
 import weakref
 import numpy as np
 import dezero.functions as F
-from dezero import cuda
+#from dezero import cuda
 from dezero.core import Parameter
-from dezero.utils import pair
+#from dezero.utils import pair
 
 
 # =============================================================================
@@ -18,3 +18,61 @@ class Layer:
         if isinstance(value, (Parameter, Layer)):
             self._params.add(name)
         super().__setattr__(name, value)
+        
+    def __call__(self, *inputs):
+        outputs = self.forward(*inputs)
+        if not isinstance(outputs, tuple):
+            outputs = (outputs,)
+        self.inputs = [weakref.ref(x) for x in inputs]
+        self.outputs = [weakref.ref(y) for y in outputs]
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, inputs):
+        raise NotImplementedError()
+
+    def params(self):
+        for name in self._params:
+            yield self.__dict__[name]
+
+            # if isinstance(obj, Layer):
+            #     yield from obj.params()
+            # else:
+            #     yield obj
+
+    def cleargrads(self):
+        for param in self.params():
+            param.cleargrad()
+            
+            
+# =============================================================================
+# Linear / Conv2d / Deconv2d
+# =============================================================================
+class Linear(Layer):
+    def __init__(self, out_size, nobias=False, dtype=np.float32, in_size=None):
+        super().__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.dtype = dtype
+
+        self.W = Parameter(None, name='W')
+        if self.in_size is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_size, dtype=dtype), name='b')
+
+    def _init_W(self, xp=np):
+        I, O = self.in_size, self.out_size
+        W_data = xp.random.randn(I, O).astype(self.dtype) * np.sqrt(1 / I)
+        self.W.data = W_data
+
+    def forward(self, x):
+        if self.W.data is None:
+            self.in_size = x.shape[1]
+            xp = cuda.get_array_module(x)
+            self._init_W(xp)
+
+        y = F.linear(x, self.W, self.b)
+        return y
